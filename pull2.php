d66a5ae5d18f8c91d1dd45841500b09f190ea08b
Rushabh Parmar <43805620+RushabhParmar12@users.noreply.github.com>
2020-02-03 12:52:56 +0530

    Create pull2.php

---
+++ pull2.php
@@ -0,0 +1,9 @@
+function execPrint($command) {
+    $result = array();
+    exec($command, $result);
+    foreach ($result as $line) {
+        print($line . "\n");
+    }
+}
+// Print the exec output inside of a pre element
+print("<pre>" . execPrint("git status") . "</pre>");
